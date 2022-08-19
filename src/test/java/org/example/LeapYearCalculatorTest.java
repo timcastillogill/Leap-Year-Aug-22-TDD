c1a2f4c68c4c27cb6a3fbcf5a9162a2ac9eddfa4
timcastillogill <timgillviola@gmail.com>
2022-08-19 10:30:10 +0100

    Refactor: created method for test

--- src/test/java/org/example/LeapYearCalculatorTest.java
+++ src/test/java/org/example/LeapYearCalculatorTest.java
@@ -1,25 +1,29 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeapYearCalculatorTest {
-
 	@Test
 	public void
 	divisible_by_four_returns_true() {
-		assertTrue(new LeapYearCalculator().checkYear(4));
+		assertTrue(leapYear(4));
 	}
 
 	@Test
 	public void
 	not_divisible_by_four_returns_false() {
-		assertFalse(new LeapYearCalculator().checkYear(3));
+		assertFalse(leapYear(3));
 	}
 	@Test
 	public void
-	leap_years_divisible_by_4_and_are_not_divisible_by_400() {
-		assertFalse(new LeapYearCalculator().checkYear(1800));
+	leap_years_if_not_divisible_by_100_and_is_divisible_by_400() {
+		assertFalse(leapYear(1800));
+	}
+
+	private boolean leapYear(int year) {
+		return new LeapYearCalculator().checkYear(year);
 	}
 }

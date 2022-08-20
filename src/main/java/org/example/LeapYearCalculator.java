83eaf85056b68c26e6bae62036bc32bc21f88ba5
MattW <mattnwarnock@yahoo.co.uk>
2022-08-20 14:33:12 +0100

    Refactor: extract divisible logic

--- src/main/java/org/example/LeapYearCalculator.java
+++ src/main/java/org/example/LeapYearCalculator.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class LeapYearCalculator {
 	public boolean checkYear(int i) {
-		return i % 4 == 0 && !(i % 100 == 0 && i % 400 != 0);
+		return divisible(i, 4) && !(divisible(i, 100) && notDivisible(i, 400));
+	}
+
+	private static boolean notDivisible(int i, int number) {
+		return i % number != 0;
+	}
+
+	private static boolean divisible(int i, int number) {
+		return i % number == 0;
 	}
 }
